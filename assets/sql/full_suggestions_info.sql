e74f659939efc25402658151960a411ab7ffaf88
Max Drushchyts <maksimk301190@gmail.com>
2022-12-14 04:57:28 +0300

    updated sql func added

--- assets/sql/full_suggestions_info.sql
+++ assets/sql/full_suggestions_info.sql
@@ -18,3 +18,22 @@ suggestion_id in (select suggestion_id from subscribed_user_relation where user_
 suggestion_id in (select suggestion_id from voted_user_relation where user_id = $1) as is_voted
 from suggestions;
 $$ LANGUAGE SQL;
+
+CREATE FUNCTION full_suggestions_info_v1() RETURNS TABLE(
+  suggestion_id int,
+  title text,
+  description text,
+  labels text [ ],
+  images text [ ],
+  author_id text,
+  status text,
+  anonymous boolean,
+  creation_time timestamp,
+  voted_user_ids text [],
+  notify_user_ids text []
+) AS $$
+  select *, 
+    (select ARRAY_AGG(user_id) from voted_user_relation where suggestion_id = suggestions.suggestion_id group by suggestion_id) as voted_user_ids,
+    (select ARRAY_AGG(user_id) from subscribed_user_relation where suggestion_id = suggestions.suggestion_id group by suggestion_id) as notify_user_ids
+      from suggestions
+$$ LANGUAGE SQL;
