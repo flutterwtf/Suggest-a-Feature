3ddf936c3667490289fc499bfc7f6a4c4e2c800b
KirillSergeevich <32823028+KirillSergeevich@users.noreply.github.com>
2023-03-25 07:44:00 +0000

    Deploying to gh-pages from @ What-the-Flutter/Suggest-a-Feature@ca752db53e3bf532b0811819a9e5346fe449d70e 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "d212d3125d698705f8440415189571c2",
+"main.dart.js": "f4860d4e4c10da8e2dd3e0aee2fd45f6",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -35,8 +35,8 @@ const RESOURCES = {
 "assets/packages/suggest_a_feature/assets/download_icon.svg": "2d71cf4bfdc8cf4c5dd9d5acb8c7a92f",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/NOTICES": "ffab37b8062874c3c2f5dcafcc466352",
-"index.html": "9a00c6698c235455a953483287e8e9f7",
-"/": "9a00c6698c235455a953483287e8e9f7",
+"index.html": "4377dd34b6ca10cf0203daff445a731d",
+"/": "4377dd34b6ca10cf0203daff445a731d",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
