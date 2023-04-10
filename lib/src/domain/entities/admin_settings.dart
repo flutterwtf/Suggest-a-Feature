ed537644949a53fa7cdb85817af96e7fca459abb
losich.maksim <losych.maksim@gmail.com>
2023-04-10 09:34:28 +0300

    doc-updates/update docs

--- lib/src/domain/entities/admin_settings.dart
+++ lib/src/domain/entities/admin_settings.dart
@@ -1,7 +1,7 @@
 import 'package:suggest_a_feature/src/domain/entities/suggestion_author.dart';
 
 /// This class is required to provide Admin information for other users
-/// In can be visible in comments
+/// It can be visible in comments
 class AdminSettings extends SuggestionAuthor {
   const AdminSettings({
     required super.id,
