f6a72ae6d7aea51b3738b2be0be0daa0b05fc542
KirillSergeevich <32823028+KirillSergeevich@users.noreply.github.com>
2025-07-17 08:30:33 +0000

    Deploying to gh-pages from @ flutterwtf/Suggest-a-Feature@7529dd3ff944b56715062daa959436dd73410468 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,11 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde9a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524b2","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "773244255"
+    serviceWorkerVersion: "2938516679"
   }
 });
